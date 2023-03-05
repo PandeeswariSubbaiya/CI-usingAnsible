789c3a2156ccded2c6eb9256cdcf6cfa6592956e
PandeeswariSubbaiya <124767670+PandeeswariSubbaiya@users.noreply.github.com>
2023-03-05 10:12:07 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,15 @@
 resource "aws_instance" "myec2" {
-  ami                    = "ami-08df646e18b182346"
+  ami                    = "ami-0f8ca728008ff5af4"
   instance_type          = "t2.micro"
   availability_zone = "ap-south-1a"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  key_name = "pswain"
+  key_name = "Mumbai_Region"
 
   tags = {
     name = "testec2"
   }
 
-  provisioner "local-exec" {
-    command = "echo ${aws_instance.myec2.public_ip} >> /etc/ansible/hosts"
-  }
+ //provisioner "local-exec" {
+    //command = "echo ${aws_instance.myec2.public_ip} >> /etc/ansible/hosts"
+  //}
 }
