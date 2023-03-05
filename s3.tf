125ba6f087a6f9173d2725da8511847055ebb671
PandeeswariSubbaiya <124767670+PandeeswariSubbaiya@users.noreply.github.com>
2023-03-05 22:33:34 +0530

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,22 @@
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "pandeeswari-terraform-statefile"
+  acl    = "public-read"
+  website {
+    index_document = "hello.html"
+   routing_rules = <<EOF
+[{
+    "Condition": {
+        "KeyPrefixEquals": "docs/"
+    },
+    "Redirect": {
+        "ReplaceKeyPrefixWith": "documents/"
+    }
+}]
+EOF
+  }
+
+  tags= {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
