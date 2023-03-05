2ab87087c01e075cecf49de61167259aa6d66572
PandeeswariSubbaiya <124767670+PandeeswariSubbaiya@users.noreply.github.com>
2023-03-05 21:51:56 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "pandeeswari-terraform-statefile"
-    key = "server_name/statefile"
+    key = "Mumbai_Region/statefile"
     region = "ap-south-1"
   }
-} 
-    s3 backend = "yes"
+}
