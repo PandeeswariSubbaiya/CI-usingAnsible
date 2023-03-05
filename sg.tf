f9178110c5b3065f669227e3c724c683b5fdc312
PandeeswariSubbaiya <124767670+PandeeswariSubbaiya@users.noreply.github.com>
2023-03-05 10:07:48 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,6 +1,6 @@
 //Security group creation and whitelisting the ip
 resource "aws_security_group" "allow_tls" {
-  name = "terraform-sg"
+  name = "terraform-sg1"
 
   ingress {
     description = "Allow port 22 - inbound"
