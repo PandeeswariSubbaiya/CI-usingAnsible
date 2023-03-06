48b554eb41857a7c4edaefe3f6184b62cb844dd1
PandeeswariSubbaiya <124767670+PandeeswariSubbaiya@users.noreply.github.com>
2023-03-06 07:49:54 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,19 +1,19 @@
 resource "aws_s3_bucket" "mybucket" {
   bucket = "pandeeswari-terraform-statefile"
   acl    = "public-read"
-  website {
-    index_document = "hello.html"
-   routing_rules = <<EOF
-[{
-    "Condition": {
-        "KeyPrefixEquals": "docs/"
-    },
-    "Redirect": {
-        "ReplaceKeyPrefixWith": "documents/"
-    }
-}]
+  //website {
+    //index_document = "hello.html"
+  // routing_rules = <<EOF
+//[{
+    //"Condition": {
+        //"KeyPrefixEquals": "docs/"
+  //  },
+   // "Redirect": {
+       // "ReplaceKeyPrefixWith": "documents/"
+   // }
+//}]
 EOF
-  }
+  }//
 
   tags= {
     Name        = "My bucket"
