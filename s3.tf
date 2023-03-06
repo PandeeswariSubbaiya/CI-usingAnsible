d312490a73b10387574e4e81189099ba41888f26
PandeeswariSubbaiya <124767670+PandeeswariSubbaiya@users.noreply.github.com>
2023-03-06 07:52:12 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,8 +1,6 @@
 resource "aws_s3_bucket" "mybucket" {
   bucket = "pandeeswari-terraform-statefile"
   acl    = "public-read"
-      key = "Mumbai_Region/statefile"
-    region = "ap-south-1"
   //website {
     //index_document = "hello.html"
   // routing_rules = <<EOF
@@ -14,7 +12,7 @@ resource "aws_s3_bucket" "mybucket" {
        // "ReplaceKeyPrefixWith": "documents/"
    // }
 //}]
-EOF
+//EOF
   }//
 
   tags= {
