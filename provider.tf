fe302fc37eebeb95ae2164bae0a9cd015edabed6
PandeeswariSubbaiya <124767670+PandeeswariSubbaiya@users.noreply.github.com>
2023-03-06 15:36:00 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,6 +9,8 @@ terraform {
 
 provider "aws" {
   region     = "ap-south-1"
+  AWS_ACCESS_KEY_ID = "AKIAUEYPFBJKFKE66SOM"
+  AWS_SECRET_ACCESS_KEY = "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"
 }
 
 terraform {
