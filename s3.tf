0bcbd41ff03e8334213f5bd328758861ccfb80d2
PandeeswariSubbaiya <124767670+PandeeswariSubbaiya@users.noreply.github.com>
2023-03-06 07:56:40 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,22 +1,6 @@
-resource "aws_s3_bucket" "mybucket" {
-  backend "s3" {
-  bucket = "pandeeswari-terraform-statefile"
-    region = "ap-south-1"
+resource "aws_s3_bucket" "mybucket" {"
   acl    = "public-read"
-  //website {
-    //index_document = "hello.html"
-  // routing_rules = <<EOF
-//[{
-    //"Condition": {
-        //"KeyPrefixEquals": "docs/"
-  //  },
-   // "Redirect": {
-       // "ReplaceKeyPrefixWith": "documents/"
-   // }
-//}]
-//EOF
-  }//
-
+  key = "Mumbai_Region/statefile"
   tags= {
     Name        = "My bucket"
     Environment = "Dev"
