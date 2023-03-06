9eae2ebe21a60bdc303b0f6b80c7a0962656e224
PandeeswariSubbaiya <124767670+PandeeswariSubbaiya@users.noreply.github.com>
2023-03-06 11:31:31 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,13 +8,15 @@ terraform {
 }
 
 provider "aws" {
+  access_key:"AKIAUEYPFBJKFKE66SOM"
+  secret_key:"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"
   region     = "ap-south-1"
 }
 
-//terraform {
-  //backend "s3" {
-    //bucket = "pandeeswari-terraform-statefile"
-   // key = "Mumbai_Region/statefile"
-  //  region = "ap-south-1"
- // }
-//}
+terraform {
+  backend "s3" {
+    bucket = "pandeeswari-terraform-statefile"
+   key = "Mumbai_Region/statefile"
+   region = "ap-south-1"
+ }
+}
