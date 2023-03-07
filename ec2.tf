8920d35945d86106d34182ed7c531b4b1e4f475e
PandeeswariSubbaiya <124767670+PandeeswariSubbaiya@users.noreply.github.com>
2023-03-07 11:35:01 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -8,33 +8,30 @@ resource "aws_instance" "myec2-NorthVirgina" {
   tags = {
     name = "testec2"
   }
-  //provisioner "file" {
-        //source = "/home/ubuntu/testfile.txt"
-        //destination = "/home/ubuntu/testfile.txt"
-//}
-       // connection {
-               // type = "ssh"
-              //  host = self.public_ip
-              //  user = "root"
-               // private_key = ("/home/ubuntu/.ssh/id_rsa")
-               // private_key = file("/home/ubuntu/.ssh/authorized_keys")
-               // timeout = "1m"
-//}
-   connection {
-    type = "ssh"
-    host = self.public_ip
-    user = "ubuntu"
-    private_key = ("/home/ubuntu/aws_iny_lappi.pem")
-    }
-   provisioner "file" {
-    source      = "index.html"
-    destination = "/home/ubuntu/index.html"
+  //Security group creation and whitelisting the ip
+resource "aws_security_group" "allow_tls" {
+  name = "terraform-sg-Nvirginia"
+
+  ingress {
+    description = "Allow port 22 - inbound"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-}
- //provisioner "local-exec" {
-    //command = "echo ${aws_instance.myec2.public_ip} >> /etc/ansible/hosts"
-  //}
-resource "aws_key_pair" "deployer" {
-  key_name = "var.North_Virginia_Keypair"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCdIKXg/PwWRqk4q5kRIRHckdhyaJLhLWGDdyaZqt9sS8kgPjFkdlD/xJplQQLddSeZfbmjQ1AgQJShidIz/1Vv9jyEQzYlmYc/poH/w8D9VzMSyM3d0Ovocfwu+G6TZj/tE+GB5INEGxFGKcvD+opDK2qauLxxl0+YO0CGbXRu1H7EjhSnhdIues5C/Is5aWD3H9khT4KBI7GprLitTLCX6P3Xz/O6Y7qknq2dYutsvTBb33OiZ1Gska2EeoNrTwBN6XNVx3SjVowgOKIEGW1WGvu094EH/IAlBKwVIEP2anvhN2ZftJ7CMJwjtGDigr9AbL5U4rJoxB3bm6uDdgL+jVkzhzuDqEYK/444ITa+O/SdumMiGmjBxdo5/RF5purzMYnH8mX+qaCdMLJocLqP32tXLbeTfyum6dAzBCh8Rv5Q6qG0EEDoTa3rnabAycfDC518RwptHXoOd1dmrrXQeOg1rN4bVokxm4O6olZHkzctmG5RnE3oldBAbqmx3K0= ubuntu@Jenkins"
+  ingress {
+    description = "Allow port 8080 - inbound"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
